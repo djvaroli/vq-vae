7e27b3a8d3980c7e95a0bd72e5ff5566fff10c80
Daniel Varoli <daniel.varoli@gmail.com>
2022-09-18 08:42:15 -0400

    ironed out a few bugs with full VQVAE model

--- model/codebook.py
+++ model/codebook.py
@@ -36,26 +36,44 @@ class VectorQuantizer(nn.Module):
         """Returns the dimension of ecah code vector"""
         return self._code_vector_dim
 
-    def quantize(self, vectors: torch.Tensor) -> torch.Tensor:
+    @property
+    def codebook_vectors(self) -> torch.Tensor:
+        return self.codebook.weight.data
+    
+    def measure_distance(self, vectors: torch.Tensor) -> torch.Tensor:
+        """Returns a tensor of shape (n_input_vectors, n_codebook_vectors) 
+        of pairwise distances between every input vector and every codebook vector.
+        """
+        return self._distance_f(vectors, self.codebook.weight)
+
+    def index_vectors(self, vectors: torch.Tensor) -> torch.Tensor:
+        """Returns the indices of the code vectors that are closest to the 
+        input vectors.
+        """
         # measure the distance between each input vector and each code vector
-        distances = self._distance_f(vectors, self.codebook.weight)
+        distances = self.measure_distance(vectors)
 
         # gives us index of closest codevector to each input vector
         encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)  # (n_x, 1)
+        return encoding_indices
+
+    def quantize(self, vectors: torch.Tensor) -> torch.Tensor:
+        encoding_indices = self.index_vectors(vectors)
+
         encodings_placeholder = torch.zeros(
             encoding_indices.shape[0], self.n_codes, device=vectors.device
         )
         encodings = encodings_placeholder.scatter(1, encoding_indices, 1)
 
         # perform the quantization step and reshape to original shape
-        quantized_inputs = torch.matmul(encodings, self.codebook.weight)
+        quantized_inputs = torch.matmul(encodings, self.codebook_vectors)
 
         return quantized_inputs
 
-    def forward(self, vectors: torch.Tensor):
+    def forward(self, inputs: torch.Tensor):
         # inputs will have shape (B, C, H, W) convert to (B, H, W, C)
-        inputs = self.rearrange_layer(vectors).contiguous()
         input_shape = inputs.shape
+        inputs = self.rearrange_layer(inputs).contiguous()
 
         # flatten inputs, essentially unrolling into a matrix of vectors
         unrolled_inputs = inputs.view((-1, self.code_vector_dim))
--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
-from turtle import forward
+import typing as t
+
 import torch
 from torch import nn
 
@@ -49,8 +50,56 @@ class ConvVQVAE(nn.Module):
             out_channels=img_channels
         )
     
-    def encode(self, inputs: torch.Tensor) -> torch.Tensor:
-        return self.pre_codebook_conv(self.encoder(inputs))
+    @property
+    def n_codes(self) -> int:
+        return self.quantizer.n_codes
+    
+    @property
+    def code_vector_dim(self) -> int:
+        return self.quantizer.code_vector_dim
+    
+    @property
+    def codebook_vectors(self) -> torch.Tensor:
+        return self.quantizer.codebook_vectors
+
+    def get_code_vectors(self, indices: torch.Tensor) -> torch.Tensor:
+        indices = indices.to(self.codebook_vectors.device)
+        return torch.index_select(self.codebook_vectors, dim=0, index=indices)
+    
+    def get_random_code_vectors(
+        self, 
+        n_code_vectors: int, 
+        seed: t.Optional[int] = None
+    ) -> torch.Tensor:
+        if seed is not None:
+            torch.manual_seed(seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed(seed)
+        
+        code_vector_indices = torch.randint(0, self.n_codes, (n_code_vectors, )).int()
+        code_vectors = self.get_code_vectors(code_vector_indices)
+
+        return code_vectors
+    
+    def _index(self, inputs: torch.Tensor) -> torch.Tensor:
+        batch_size = inputs.shape[0]
+        flat_inputs = inputs.view((-1, self.code_vector_dim))
+        code_vector_indices = self.quantizer.index_vectors(flat_inputs)
+        return code_vector_indices.view((batch_size, -1))
+
+    def index(self, inputs: torch.Tensor) -> torch.Tensor:
+        encoded = self.encode(inputs)
+        return self._index(encoded)
+    
+    def encode(
+        self, 
+        inputs: torch.Tensor,
+    ) -> torch.Tensor:
+        pre_quantized = self.pre_codebook_conv(self.encoder(inputs))
+        return pre_quantized
+    
+    def encode_and_quantize(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.quantize(self.encode(inputs))
     
     def quantize(self, inputs: torch.Tensor) -> torch.Tensor:
         return self.quantizer(inputs)
