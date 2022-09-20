b7c26f4bdb9f5d2605ddbbbaafafccc7c0a95cfa
Daniel Varoli <daniel.varoli@gmail.com>
2022-09-19 21:21:58 -0400

    Added VQVAE trainer and training script

--- model/trainer.py
+++ model/trainer.py
@@ -1,4 +1,5 @@
 import typing as t
+from collections import deque
 
 import torch 
 from torch import nn
@@ -17,7 +18,6 @@ class VQVAETrainer:
         optimizer_factory: t.Callable[[t.Iterator[nn.Parameter]], torch.optim.Optimizer],
         quantization_loss_fn: nn.Module = QuantizationLoss(0.25),
         reconstruction_loss_fn: nn.Module = torch.nn.MSELoss(),
-        dataset_variance: t.Optional[float] = 1.,
         metric_update_steps: int = 25,
         device: t.Optional[torch.device] = None
     ) -> None:
@@ -28,11 +28,57 @@ class VQVAETrainer:
         self.metric_update_steps = metric_update_steps
         self.device = device
         self.model.to(self.device)
-    
+        self.quantization_loss_history = deque()
+        self.reconstruction_loss_history = deque()
+        self.loss_history = deque()
+
     def set_training_device(self, device: torch.device):
         self.model.to(device)
         self.device = device
     
+    def _train_single_epoch(
+        self, 
+        dataloader: DataLoader,
+        progress_bar: tqdm,
+        scale_reconstruction_loss: bool = True
+    ):
+        for batch_idx, data_batch in enumerate(dataloader):
+            if not isinstance(data_batch, torch.Tensor):
+                data_batch = data_batch[0]
+            
+            data_batch = data_batch.to(self.device)
+            
+            encoded = self.model.encode(data_batch)
+            quantized = self.model.quantize(encoded)
+
+            quantization_loss = self.quantization_loss_fn(encoded, quantized)
+
+            # this trick allows us to pass decoder gradients to encoder through the quantization step
+            quantized = encoded + (quantized - encoded).detach()
+            
+            reconsutruction = self.model.decode(quantized)
+
+            reconstruction_loss = self.reconstruction_loss_fn(data_batch, reconsutruction)
+
+            if scale_reconstruction_loss:
+                reconstruction_loss /= np.var(dataloader.dataset.data / 255.0)
+
+            loss = reconstruction_loss + quantization_loss
+            loss.backward()
+            self.optimizer.step()
+            
+            progress_bar.update()
+
+            if batch_idx % self.metric_update_steps == 0:
+                progress_bar.set_postfix(dict(
+                    ReconstructionLoss=reconstruction_loss.detach().cpu().numpy(),
+                    QuantizationLoss=quantization_loss.detach().cpu().numpy()
+                ))
+            
+            self.quantization_loss_history.append(quantization_loss.detach().cpu().numpy())
+            self.reconstruction_loss_history.append(reconstruction_loss.detach().cpu().numpy())
+            self.loss_history.append(loss.detach().cpu().numpy())
+
     def train(
         self, 
         dataloader: DataLoader,
@@ -42,37 +88,5 @@ class VQVAETrainer:
         for epoch in range(1, n_epochs + 1):
             progress_bar = tqdm(total=len(dataloader))
             progress_bar.set_description(f"Epoch {epoch} / {n_epochs}")
-            for batch_idx, data_batch in enumerate(dataloader):
-                if not isinstance(data_batch, torch.Tensor):
-                    data_batch = data_batch[0]
-                
-                data_batch = data_batch.to(self.device)
-                
-                encoded = self.model.encode(data_batch)
-                quantized = self.model.quantize(encoded)
-
-                quantization_loss = self.quantization_loss_fn(encoded, quantized)
-
-                # this trick allows us to pass decoder gradients to encoder through the quantization step
-                quantized = encoded + (quantized - encoded).detach()
-                
-                reconsutruction = self.model.decode(quantized)
-
-                reconstruction_loss = self.reconstruction_loss_fn(data_batch, reconsutruction)
-
-                if scale_reconstruction_loss:
-                    reconstruction_loss /= np.var(dataloader.dataset.data / 255.0)
-
-                loss = reconstruction_loss + quantization_loss
-                loss.backward()
-                self.optimizer.step()
-                
-                progress_bar.update()
-
-                if batch_idx % self.metric_update_steps == 0:
-                    progress_bar.set_postfix(dict(
-                        ReconstructionLoss=reconstruction_loss.detach().cpu().numpy(),
-                        QuantizationLoss=quantization_loss.detach().cpu().numpy()
-                    ))
-            
+            self._train_single_epoch(dataloader, progress_bar, scale_reconstruction_loss)
             progress_bar.close()
---
+++ train_vqvae.py
@@ -0,0 +1,65 @@
+from multiprocessing.resource_sharer import stop
+import typing as t
+from argparse import ArgumentParser
+from enum import Enum
+import os
+
+import torch
+from torchvision.utils import make_grid
+import neptune.new as neptune
+from neptune.new.types import File
+from tqdm import tqdm
+
+from model import VQVAE, VQVAETrainer
+from data import CIFAR10DataLoader
+
+
+class DataloaderOptions(Enum):
+    CIFAR10 = CIFAR10DataLoader
+
+
+def train_vqvae(
+):
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps:0")
+    else:
+        device = torch.device("cpu")
+    
+    print(f'Using device: {device}.')
+
+    n_epochs = 2
+
+    project_name = os.environ["NEPTUNE_PROJECT_NAME"]
+    api_token = os.environ["NEPTUNE_API_TOKEN"]
+    run = neptune.init(
+        project=project_name,
+        api_token=api_token
+    )
+
+    dataloader = DataloaderOptions.CIFAR10.value()
+    optimizer_factory = lambda params: torch.optim.Adam(params)
+    model = VQVAE(3)
+    trainer = VQVAETrainer(model, optimizer_factory, device=device)
+
+    test_samples = dataloader.get_sample_images(8, seed=1000).to(device)
+    test_reconstructions = model(test_samples).detach().cpu()
+    test_reconstructions_grid = make_grid(test_reconstructions, nrow=4).permute(1, 2, 0).numpy()
+
+    run["reconstruction-epoch-0"].log(File.as_image(test_reconstructions_grid))
+
+    for epoch in range(1, n_epochs + 1):
+        progress_bar = tqdm(total=len(dataloader))
+        progress_bar.set_description(f"Epoch {epoch} / {n_epochs}")
+        trainer._train_single_epoch(dataloader, progress_bar)
+
+        test_reconstructions = model(test_samples).detach().cpu()
+        test_reconstructions_grid = make_grid(test_reconstructions, nrow=4).permute(1, 2, 0).numpy()
+
+        run[f"reconstruction-epoch-{epoch}"].log(File.as_image(test_reconstructions_grid))
+    
+    run.stop()
+
+if __name__ == "__main__":
+    train_vqvae()
