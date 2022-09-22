63b9988647bf6a12d1706af906737df0755cd03a
Daniel Varoli <daniel.varoli@gmail.com>
2022-09-21 22:56:25 -0400

    added batch_size parameter to vqvae training script

--- train_vqvae.py
+++ train_vqvae.py
@@ -26,6 +26,7 @@ class DataloaderOptions(Enum):
 class TrainingParameters:
     dataset_name: str
     n_epochs: int
+    batch_size: int
     device_id: str
     seed: t.Optional[int] = None
 
@@ -33,6 +34,7 @@ class TrainingParameters:
 def train_vqvae(
     n_epochs: int,
     device_id: str,
+    batch_size: int,
     seed: t.Optional[int] = None
 ):
     device = torch.device(device_id)
@@ -48,14 +50,18 @@ def train_vqvae(
         dataset_name="CIFAR10",
         n_epochs=n_epochs,
         device_id=device_id,
+        batch_size=batch_size,
         seed=seed
     )
-    dataloader = DataloaderOptions[run_parameters.dataset_name].value()
+    dataloader = DataloaderOptions[run_parameters.dataset_name].value(
+        batch_size=run_parameters.batch_size
+    )
     optimizer_factory = lambda params: torch.optim.Adam(params)
 
     run["parameters"] = run_parameters.__dict__
 
     model = VQVAE(3)
+    model.to(device)
     trainer = VQVAETrainer(model, optimizer_factory, device=device)
 
     test_samples = dataloader.get_sample_images(8, seed=run_parameters.seed).to(device)
@@ -81,11 +87,13 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-e", "--n_epochs", required=True, type=int, help="Number of epochs to train model for.")
     parser.add_argument("-d", "--device_id", required=True, type=str, help="Device to run model training on.")
+    parser.add_argument("-b", "--batch_size", type=int, default=128, help="Size of each training batch.")
     parser.add_argument("--seed", type=int, default=None, help="Random seed for stochastic operations.")
     args = parser.parse_args()
 
     train_vqvae(
         n_epochs=args.n_epochs,
         device_id=args.device_id,
-        seed=args.seed
+        seed=args.seed,
+        batch_size=args.batch_size
     )
